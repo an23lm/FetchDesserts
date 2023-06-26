5e2599d6f38e0c3df1a9f69307a92fc3ad7e9d77
Anselm Joseph <anselmjs@icloud.com>
2023-06-26 00:39:24 -0700

    Initial Commit

---
+++ FetchDesserts/FetchDessertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertsApp.swift
+//  FetchDesserts
+//
+//  Created by Anselm Joseph on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
